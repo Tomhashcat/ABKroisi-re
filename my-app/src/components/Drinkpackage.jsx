94773618eab82f322b68721e60fe0ed740466ecf
Tomhashcat <dutourne.t@example.com>
2024-09-16 21:11:29 +0200

    fdsbhrfh

--- my-app/src/components/Drinkpackage.jsx
+++ my-app/src/components/Drinkpackage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import drinkPackagesMSC from "../drink_packages_MSC.json"; // Forfaits MSC
 import drinkPackagesCosta from "../drink_Packages_Costa.json"; // Forfaits Costa
-import drinkPackagesRCCL from "../drink_Packages_RCCL.json"
+import drinkPackagesRCCL from "../drink_Packages_RCCL.json"; // Forfaits RCCL
 import drinkPackagesPonant from "../drink_Packages_Ponan.json"; // Forfaits Ponant
 import drinkPackagesCelestyal from "../drink_Pacjages_celestyal.json"; // Forfaits Celestyal
 
@@ -11,16 +11,24 @@ const DrinkPackages = () => {
 
   useEffect(() => {
     // Charger les forfaits en fonction de la compagnie sélectionnée
-    if (selectedCompany === "MSC") {
-      setPackages(drinkPackagesMSC);
-    } else if (selectedCompany === "Costa") {
-      setPackages(drinkPackagesCosta);
-    } else if (selectedCompany === "RCCL") {
-      setPackages(drinkPackagesRCCL);
-    } else if (selectedCompany === "Ponant") {
-      setPackages(drinkPackagesPonant);
-    } else if (selectedCompany === "Celestyal") {
-      setPackages(drinkPackagesCelestyal);
+    switch (selectedCompany) {
+      case "MSC":
+        setPackages(drinkPackagesMSC);
+        break;
+      case "Costa":
+        setPackages(drinkPackagesCosta);
+        break;
+      case "RCCL":
+        setPackages(drinkPackagesRCCL);
+        break;
+      case "Ponant":
+        setPackages(drinkPackagesPonant);
+        break;
+      case "Celestyal":
+        setPackages(drinkPackagesCelestyal);
+        break;
+      default:
+        setPackages({});
     }
   }, [selectedCompany]);
 
@@ -46,15 +54,15 @@ const DrinkPackages = () => {
       </div>
 
       {/* Affichage des forfaits si une compagnie est sélectionnée */}
-      {selectedCompany && packages[selectedCompany] && (
+      {selectedCompany && packages && (
         <div className="packages-list">
           <h2>Forfaits disponibles pour {selectedCompany}</h2>
 
           {/* Lien principal vers les forfaits de la compagnie */}
-          {packages[selectedCompany].link && (
+          {packages.link && (
             <div className="main-link">
               <a
-                href={packages[selectedCompany].link}
+                href={packages.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="view-all-link"
@@ -65,24 +73,23 @@ const DrinkPackages = () => {
           )}
 
           {/* Boucle sur les forfaits de la compagnie */}
-          {Object.keys(packages[selectedCompany])
+          {Object.keys(packages)
             .filter((key) => key !== "link") // Filtrer la clé "link" pour afficher uniquement les forfaits
             .map((packageName) => (
               <div key={packageName} className="package-card">
                 <h3 className="package-title">{packageName}</h3>
                 <div className="choix">
                   <p className="package-description">
-                    {packages[selectedCompany][packageName].description}
+                    {packages[packageName].description}
                   </p>
                   <ul className="package-inclusions">
-                    {packages[selectedCompany][packageName].inclusions.map(
-                      (item, index) => (
+                    {packages[packageName].inclusions &&
+                      packages[packageName].inclusions.map((item, index) => (
                         <li key={index}>{item}</li>
-                      )
-                    )}
+                      ))}
                   </ul>
                   <p className="package-price">
-                    {packages[selectedCompany][packageName].prix}
+                    {packages[packageName].prix}
                   </p>
                 </div>
               </div>
